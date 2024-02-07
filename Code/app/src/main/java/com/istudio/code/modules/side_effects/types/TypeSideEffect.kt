c75074a99ee2ae82f97882e355d567b83a628962
Devrath <devrath.dev595@gmail.com>
2024-02-07 20:17:37 +0530

    new summary added

--- Code/app/src/main/java/com/istudio/code/modules/side_effects/types/TypeSideEffect.kt
+++ Code/app/src/main/java/com/istudio/code/modules/side_effects/types/TypeSideEffect.kt
@@ -1,9 +1,11 @@
 package com.istudio.code.modules.side_effects.types
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -14,7 +16,11 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
@@ -25,27 +31,82 @@ fun TypeSideEffect(navController: NavHostController) {
     // Use SideEffect to log the current value of count
     SideEffect {
         // Called on every recomposition
-        println("Outer Count is ${count.value}")
+        println("Root Side effect is triggered")
     }
+
+    println("Root composable is composed")
 
     Column(
         modifier = Modifier
+            .background(color = Color.Blue)
             .fillMaxSize()
-            .padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+            .padding(30.dp)
     ) {
 
-        println("Column composable is composed")
+        println("(BLUE) composable is composed")
 
-        Text(text = "${count.value}")
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { count.value++ }) {
-            println("Button composable is composed")
-            Text(text = "Increase count")
+        SideEffect {
+            // Called on every recomposition
+            println("Blue composable Side effect is triggered")
         }
 
+        Column(
+            modifier = Modifier
+                .weight(1f).fillMaxWidth()
+                .height(100.dp)
+                .background(Color.Green),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            println("(GREEN) composable is composed")
+
+            SideEffect {
+                // Called on every recomposition
+                println("Green composable Side effect is triggered")
+            }
+
+            Text(
+                text = "${count.value}",
+                color = Color.Magenta,
+                fontSize = 38.sp
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Button(onClick = { count.value++ }) {
+                println("Button composable is composed")
+                Text(text = "Increase count")
+
+                SideEffect {
+                    // Called on every recomposition
+                    println("Button composable Side effect is triggered")
+                }
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(1f).fillMaxWidth()
+                .height(100.dp)
+                .background(Color.Red),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            println("(RED) composable is composed")
+
+            SideEffect {
+                // Called on every recomposition
+                println("Red composable Side effect is triggered")
+            }
+        }
     }
+
+}
+
+@Preview
+@Composable
+fun Preview(){
+    TypeSideEffect(navController = NavHostController(LocalContext.current))
 }
