39493223d0026c020172952429a1d0f068219505
devrath dev <devrath.dev595@gmail.com>
2024-05-06 19:04:45 +0530

    changes

--- CodeSamples/app/src/main/java/com/istudio/code/modules/app_foundations/AppFoundationsActivity.kt
+++ CodeSamples/app/src/main/java/com/istudio/code/modules/app_foundations/AppFoundationsActivity.kt
@@ -3,10 +3,20 @@ package com.istudio.code.modules.app_foundations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.istudio.code.module_selection.ModuleDemo
+import com.istudio.code.module_selection.ModuleMainSelectionScreen
+import com.istudio.code.modules.app_foundations.demo_selection.AppFoundationsDemo
 import com.istudio.code.modules.app_foundations.helpers.rememberWindowSizeClass
 import com.istudio.code.modules.app_foundations.demo_selection.AppFoundationsSelectionScreen
 import com.istudio.code.modules.app_foundations.theme.CustomTheme
+import com.istudio.code.ui.theme.CodeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,9 +28,26 @@ class AppFoundationsActivity : ComponentActivity() {
             val window = rememberWindowSizeClass()
             CustomTheme(window) {
                 val navController = rememberNavController()
-                AppFoundationsSelectionScreen( navController = navController)
+                AppFoundationsSelectionScreen(navController = navController)
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = AppFoundationsDemo.MainSelection.rout
+                    ) {
+                        // Selection Screen
+                        composable(AppFoundationsDemo.MainSelection.rout) {
+                            AppFoundationsSelectionScreen(navController = navController)
+                        }
+                    }
+                }
             }
         }
     }
-
 }
