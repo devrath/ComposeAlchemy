0ada4c260f3562ace2eb38cb40b26f62e64fde79
Devrath <devrath.dev595@gmail.com>
2024-12-21 13:06:54 +0530

    added

--- CodeSamples/app/src/main/java/com/istudio/code/modules/basic_layouts/modules/BoxDemo.kt
+++ CodeSamples/app/src/main/java/com/istudio/code/modules/basic_layouts/modules/BoxDemo.kt
@@ -1,29 +1,24 @@
 package com.istudio.code.modules.basic_layouts.modules
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.istudio.code.R
 import com.istudio.code.ui.theme.CodeTheme
-import com.istudio.code.utils.setWidthMode
 
 @Composable
 fun BoxDemo(navController: NavHostController) {
@@ -33,15 +28,37 @@ fun BoxDemo(navController: NavHostController) {
 @Composable
 fun BoxDemoScreen(modifier: Modifier = Modifier) {
 
-    Box(
-        contentAlignment = Alignment.BottomEnd
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Box(modifier = Modifier
-            .size(100.dp)
-            .background(Color.Cyan))
-        Box(modifier = Modifier
-            .size(50.dp)
-            .background(Color.Magenta))
+        Box(
+            contentAlignment = Alignment.BottomEnd // We can put the alignment in the parent
+        ) {
+            // First Position = Image
+            Image(
+                painter = painterResource(R.drawable.pikachu),
+                contentDescription = "Background Image"
+            )
+            // Second Position = Gradient on top of image
+            Box(
+                modifier = Modifier
+                    .matchParentSize() // Special modifier that matches to the parent content
+                    .background(
+                        // We can add the gradient here
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Magenta)
+                        )
+                    )
+            )
+            // Third position = icon on top of both Image and Gradient
+            Icon(
+                modifier = Modifier.align(alignment = Alignment.BottomCenter),// Individually we can add the alignment
+                imageVector = Icons.Default.Star,
+                contentDescription = "Star"
+            )
+        }
     }
 
 }
