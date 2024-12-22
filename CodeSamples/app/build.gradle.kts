7286b2b1dd5bc0a03994a91baa38e2fb28641a34
Devrath <166014800+devrath-teg@users.noreply.github.com>
2024-12-22 16:38:17 +0530

    added new code

--- CodeSamples/app/build.gradle.kts
+++ CodeSamples/app/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-viewbinding:1.7.6")
+    implementation("androidx.compose.material3.adaptive:adaptive-android:1.0.0")
+    implementation("androidx.compose.material3.adaptive:adaptive-desktop:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- CodeSamples/app/src/main/java/com/istudio/code/modules/basic_layouts/modules/WindowSizeDemo.kt
+++ CodeSamples/app/src/main/java/com/istudio/code/modules/basic_layouts/modules/WindowSizeDemo.kt
@@ -1,9 +1,17 @@
 package com.istudio.code.modules.basic_layouts.modules
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.window.core.layout.WindowWidthSizeClass
 import com.istudio.code.ui.theme.CodeTheme
 
 @Composable
@@ -14,17 +22,71 @@ fun WindowSizeDemo(navController: NavHostController) {
 @Composable
 fun WindowSizeDemoScreen(modifier: Modifier = Modifier) {
 
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+
+            when (windowSizeClass.windowWidthSizeClass) {
+                WindowWidthSizeClass.COMPACT -> {
+                    Text(text = "Compact width")
+                }
+
+                WindowWidthSizeClass.MEDIUM -> {
+                    Text(text = "Medium width")
+                }
+
+                WindowWidthSizeClass.EXPANDED -> {
+                    Text(text = "Expanded width")
+                }
+
+                else -> {
+                    Text(text = "Unknown width")
+                }
+            }
+        }
+    }
 
 
 }
 
 @Preview(
     showBackground = true,
-    backgroundColor = 0xFFF
+    backgroundColor = 0xFFF,
+    device = "spec:width=411dp,height=891dp"
 )
 @Composable
-private fun CurrentScreenPreview() {
+private fun CurrentScreenPhonePreview() {
     CodeTheme { WindowSizeDemoScreen() }
 }
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFF,
+    device = "spec:width=1280dp,height=800dp,dpi=240"
+)
+@Composable
+private fun CurrentScreenTabletPreview() {
+    CodeTheme { WindowSizeDemoScreen() }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFF,
+    device = "spec:width=673dp,height=841dp"
+)
+@Composable
+private fun CurrentScreenFoldablePreview() {
+    CodeTheme { WindowSizeDemoScreen() }
+}
+
 
 
