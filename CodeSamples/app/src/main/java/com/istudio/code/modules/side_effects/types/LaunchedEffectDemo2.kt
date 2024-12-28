01b010233a195dd5c0213099dd6f4eb201ad6ac7
Devrath <devrath.dev595@gmail.com>
2024-12-28 22:35:20 +0530

    added

--- CodeSamples/app/src/main/java/com/istudio/code/modules/side_effects/types/LaunchedEffectDemo2.kt
+++ CodeSamples/app/src/main/java/com/istudio/code/modules/side_effects/types/LaunchedEffectDemo2.kt
@@ -6,17 +6,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.istudio.code.ui.theme.CodeTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun LaunchedEffectDemo2(navController: NavHostController) {
@@ -27,18 +33,26 @@ fun LaunchedEffectDemo2(navController: NavHostController) {
 fun LaunchedEffectDemo2Screen(modifier: Modifier = Modifier) {
 
     var counterState by remember { mutableIntStateOf(0) }
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(counterState) {
+        if (counterState % 2 == 0) {
+            snackbarHostState.showSnackbar(" $counterState --> It is a even number")
+        }
+    }
 
     Scaffold(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) { padding ->
         Box(
-            modifier = Modifier.fillMaxSize().padding(padding),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
             contentAlignment = Alignment.Center
         ) {
             Button(
-                onClick = {
-                    counterState++
-                }
+                onClick = { counterState++ }
             ) {
                 Text(text = "Current Value -> $counterState")
             }
